64bb2fe251621b49ddb267cd9d07b88672f7f109
elizabetsm <liza_ryzhkina@hotmail.com>
2020-10-25 22:10:59 +0300

    finally norm header

--- includes/minishell.h
+++ includes/minishell.h
@@ -16,7 +16,7 @@ typedef struct	s_struct {
 	char		*inp; /*здесь хранится ввод*/
 	char		**args; /*здесь хранится строка после вводаБ разделенная по словам*/
 	int			b_trig;
-	char 		*path;
+	char 		**paths;
 	char 		**env;
 }				t_struct;
 
@@ -25,4 +25,7 @@ void	check_built(t_struct *st, char ** env);
 void	execute(t_struct *st, char **env);
 void	input(t_struct *st);
 void	cd_builtin(t_struct *st, char **env);
+void env_builtin(t_struct *st);
+void setenv_builtin(t_struct *st);
+void unsetenv_builtin(t_struct *st);
 #endif
